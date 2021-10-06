338bf777ec1a1e1c160d75e8b1ae1697208771ac
Anshu Pasrija <anshu.pasrija@gmail.com>
2021-10-06 15:15:13 -0700

    test cases

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -7,6 +7,7 @@ export default function Form(props) {
   console.log("props fro form ", props)
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
   const reset = () => {
     setName("")
     setInterviewer(null)
@@ -15,8 +16,14 @@ export default function Form(props) {
     reset()
     props.onCancel()
   }
-  const save=()=>{
-    props.onSave(name,interviewer);
+  
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+  
+    props.onSave(name, interviewer);
   }
   return (
     <main className="appointment__card appointment__card--create">
@@ -29,17 +36,16 @@ export default function Form(props) {
           placeholder="Enter Student Name"
           value={name}
           onChange={(event) => setName(event.target.value)}
-          /*
-            This must be a controlled component
-          */
+          data-testid="student-name-input"
         />
       </form>
+      <section className="appointment__validation">{error}</section>
       <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
     </section>
     <section className="appointment__card-right">
       <section className="appointment__actions">
-        <Button danger onClick ={cancel}>Cancel</Button>
-        <Button confirm onClick ={save}>Save</Button>
+        <Button danger onClick ={cancel}>Cancel</Button>     
+        <Button confirm onClick={event => validate()}>Save</Button>
       </section>
     </section>
   </main>
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -6,6 +6,8 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
+
+
   const transition = (newMode,replace=false)=>{
     if(!replace){
     setHistory((prev)=>[...prev,newMode])  // setting the history, prev is getting the mode value

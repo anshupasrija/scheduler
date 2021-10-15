5b65dd2a4f533dc2f57e4b809b961e23d801100e
Anshu Pasrija <anshu.pasrija@gmail.com>
2021-10-15 13:36:17 -0700

    final submission

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,53 +1,7 @@
-// import React from "react";
 
-// const state = {
-//   days: [
-//     {
-//       id: 1,
-//       name: "Monday",
-//       appointments: [1, 2, 3],
-//     },
-//     {
-//       id: 2,
-//       name: "Tuesday",
-//       appointments: [4, 5],
-//     },
-//   ],
-//   appointments: {
-//     1: { id: 1, time: "12pm", interview: null },
-//     2: { id: 2, time: "1pm", interview: null },
-//     3: {
-//       id: 3,
-//       time: "2pm",
-//       interview: { student: "Archie Cohen", interviewer: 2 },
-//     },
-//     4: { id: 4, time: "3pm", interview: null },
-//     5: {
-//       id: 5,
-//       time: "4pm",
-//       interview: { student: "Chad Takahashi", interviewer: 2 },
-//     },
-//   },
-// };
-
-// const interviewers = {
-//   1: {
-//     id: 1,
-//     name: "Sylvia Palmer",
-//     avatar: "https://i.imgur.com/LpaY82x.png",
-//   },
-//   2: {
-//     id: 2,
-//     name: "Tori Malcolm",
-//     avatar: "https://i.imgur.com/Nmx0Qxo.png",
-//   },
-// };
 export function getAppointmentsForDay(state, day) {
   const foundDay = state.days.find((obj) => obj.name === day);
-  if (foundDay === undefined) {
-    return [];
-  }
-  // console.log(foundDay);
+  if (foundDay === undefined) return [];
   const appointments = foundDay.appointments.map(
     (id) => state.appointments[id]
   );
